97e020be52888b82deb146b67213d65a003f401b
Wazowski <jorisdebock@outlook.com>
2020-04-18 13:33:19 +0200

    add compile paths to the open project

--- src/SmartCodeGenerator.Engine/Program.cs
+++ src/SmartCodeGenerator.Engine/Program.cs
@@ -1,24 +1,23 @@
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Build.Locator;
 
 namespace SmartCodeGenerator.Engine
 {
-    class Program
+    public class Program
     {
-        static async Task<int> Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
             {
-                Console.WriteLine(((Exception) eventArgs.ExceptionObject).ToString());
+                Console.WriteLine(((Exception)eventArgs.ExceptionObject).ToString());
             };
-
             var progressReporter = new ProgressReporter();
 
-            var options =  OptionsHelper.LoadOptions(args, progressReporter);
+            var options = OptionsHelper.LoadOptions(args, progressReporter);
             if (options == null)
             {
                 return -1;
@@ -34,11 +33,41 @@ namespace SmartCodeGenerator.Engine
             using (var workspace = MsBuildHelper.CreateMsBuildWorkspace(progressReporter))
             {
                 var project = await workspace.OpenProjectAsync(options.ProjectPath, progressReporter);
+
+                project = AddCompileDocuments(project, options.ProjectPath, options.CompilePaths);
+
                 var generatorAssemblySearchPaths = options.GetGeneratorPluginsSearchPaths(progressReporter);
                 var generator = new CompilationGenerator(generatorAssemblySearchPaths, options.OutputPath, progressReporter);
                 await generator.Process(project);
             }
             return 0;
+        }
+
+        private static Project AddCompileDocuments(Project project, string projectPath, string compilePaths)
+        {
+            if (compilePaths == null)
+            {
+                return project;
+            }
+
+            var projectDirectoryPath = Path.GetDirectoryName(projectPath);
+            foreach (var file in compilePaths.Split(";"))
+            {
+                var filePath = Path.IsPathFullyQualified(file) ? file : Path.Combine(projectDirectoryPath, file);
+                if (!File.Exists(filePath))
+                {
+                    continue;
+                }
+
+                var name = Path.GetFileName(filePath);
+                if (!project.Documents.Any(x => x.Name == name))
+                {
+                    var textDocument = project.AddDocument(name, File.ReadAllText(filePath), filePath: filePath);
+                    project = textDocument.Project;
+                }
+            }
+
+            return project;
         }
     }
 }
--- src/SmartCodeGenerator.Engine/ToolOptions.cs
+++ src/SmartCodeGenerator.Engine/ToolOptions.cs
@@ -2,7 +2,7 @@ using CommandLine;
 
 namespace SmartCodeGenerator.Engine
 {
-    class ToolOptions
+    public class ToolOptions
     {
         [Option('p', "project", Required = true, HelpText = "Project path")]
         public string ProjectPath { get; set; } = null!;
@@ -12,5 +12,8 @@ namespace SmartCodeGenerator.Engine
 
         [Option('g', "generators", Required = true, HelpText = "Generator paths")]
         public string GeneratorPaths { get; set; } = null!;
+
+        [Option('c', "compile", Required = false, HelpText = "Compile file paths")]
+        public string CompilePaths { get; set; } = null!;
     }
 }
